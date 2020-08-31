020e29c72b40cd3b3f90f9d257d0277117a1246f
Blackqwerty <rares504@gmail.com>
2020-08-31 03:02:21 +0300

    Added Buletin Informativ and fixed doctrine buttons

---
+++ dist/js/buletin-exit.js
@@ -0,0 +1,5 @@
+const exit = document.querySelector('#exit-bi');
+exit.addEventListener('click', () => {
+  exit.style.display = 'none';
+  document.querySelector('.buletin-informativ').style.display = 'none';
+});
