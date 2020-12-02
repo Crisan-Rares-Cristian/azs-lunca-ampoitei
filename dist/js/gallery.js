5e8eed40eac937a83ffbf629c0c807fcfdb4eea8
Crisan Rares-Cristian <rares504@gmail.com>
2020-12-03 00:42:18 +0200

    Changes to be committed: 	new file:   about.txt 	new file:   dist/css/buletin-informativ.css

---
+++ dist/js/gallery.js
@@ -0,0 +1,142 @@
+// //  Preload Images
+//  const preloadImages = url => {
+//    let img = new Image();
+//    img.src = url;
+//  }
+
+//  for(let i = 1; i <= 21; i++) {
+//    preloadImages(`poze/Gallery/${i}.jpg`);
+//  }
+
+
+var img = document.querySelector("#main-img");
+var doc = document.querySelector("#main-doc");
+var nrImg = 1;
+var nrDoc = 1;
+
+// Docs - add Event Listeners
+const nextDoc = document.querySelector("#next-doc"); 
+const prvDoc = document.querySelector("#prv-doc"); 
+nextDoc.addEventListener('click', goToNextDoc);
+prvDoc.addEventListener('click', goToPrvDoc);
+
+// Goes to next doc
+function goToNextDoc() {
+
+  if(nrDoc == 50) {
+    nrDoc ++;
+    doc.src = 'poze/documente/'+ nrDoc +'.jpg';
+    nextDoc.style.display = 'none';
+  }
+
+  if(nrDoc < 50) {
+    nrDoc ++;
+    doc.src = 'poze/documente/'+ nrDoc +'.jpg';
+    prvDoc.style.display = 'block';
+  }
+
+}
+
+// Goes to previous doc
+function goToPrvDoc() {
+  if(nrDoc == 2) {
+    nrDoc --;
+    doc.src = 'poze/documente/'+ nrDoc +'.jpg';
+    prvDoc.style.display = 'none';
+  }
+
+  if(nrDoc> 2) {
+    nrDoc--;
+    doc.src = 'poze/documente/'+ nrDoc +'.jpg';
+    nextDoc.style.display = 'block';
+  } 
+}
+
+if(nrDoc == 1) {
+  prvDoc.style.display = 'none';
+}
+
+// Images - add Event Listeners
+const nextImg = document.querySelector("#next-img"); 
+const prvImg = document.querySelector("#prv-img"); 
+ nextImg.addEventListener('click', goToNextImg);
+ prvImg.addEventListener('click', goToPrvImg);
+
+// Goes to next picture
+function goToNextImg() {
+
+  if(nrImg == 21) {
+    nrImg ++;
+    img.src = 'poze/Gallery/'+ nrImg +'.jpg';
+    nextImg.style.display = 'none';
+  }
+
+  if(nrImg < 21) {
+    nrImg ++;
+    img.src = 'poze/Gallery/'+ nrImg +'.jpg';
+    prvImg.style.display = 'block';
+  }
+
+}
+
+// Goes to previous picture
+function goToPrvImg() {
+  if(nrImg == 2) {
+    nrImg --;
+    img.src = 'poze/Gallery/'+ nrImg +'.jpg';
+    prvImg.style.display = 'none';
+  }
+
+  if(nrImg > 2) {
+    nrImg --;
+    img.src = 'poze/Gallery/'+ nrImg +'.jpg';
+    nextImg.style.display = 'block';
+  } 
+}
+
+if(nrImg == 1) {
+  prvImg.style.display = 'none';
+}
+
+
+// On button screen toggle fullscreen for images
+document.querySelector("#full-screen-img").addEventListener('click', goFullScr1);
+
+function goFullScr1() {
+  open('poze/Gallery/'+ nrImg +'.jpg');
+}
+
+// On button screen toggle fullscreen for docs
+document.querySelector("#full-screen-doc").addEventListener('click', goFullScr);
+
+function goFullScr() {
+  open('poze/documente/'+ nrDoc +'.jpg');
+}
+
+
+// Add Event listener on menu buttons
+var navBtns = document.querySelectorAll(".nav-item");
+var contents = document.querySelectorAll(".main-showcase");
+
+//Show the content that is related with the btn
+navBtns.forEach(item => {
+  item.addEventListener('click', function() {
+    removeShow(); //Call the show
+    var content = document.querySelector(`#${this.classList[1]}-content`); //Seek the menu-btn related content
+    content.classList.add('show'); // Adds the show class to the found content 
+    // Removes the left arrow on first doc
+    if(nrDoc == 1) {
+      prvDoc.style.display = 'none';
+    }
+
+    // Removes the left arrow on first image
+    if(nrImg == 1) {
+      prvImg.style.display = 'none';
+    }
+  });
+});
+
+// Removes the show class for all content
+function removeShow() {
+  contents.forEach(item => item.classList.remove('show'));
+}
---
+++ dist/js/main.js
@@ -0,0 +1,210 @@
+var menu = document.getElementById("menu");
+var menuItems = document.getElementsByClassName("menu-item");
+var showcase = document.getElementsByClassName("showcase");
+// Vars for drop menu
+var dropBtn = document.getElementsByClassName("drop-btn");
+var dropContent = document.getElementsByClassName("drop-content");
+var loader = document.getElementById("loader");
+
+//Media query START
+function mediaQuery(x) {
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 2000);
+
+  // Loops throught the dropBtn HTML Collection
+  for (let i = 0; i < dropBtn.length; i++) {
+    // Listens for hover
+    dropBtn[i].addEventListener("click", showContent);
+    // Shows Content on click
+    function showContent() {
+      dropContent[i].classList.toggle("show");
+    }
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (e) {
+      if (!e.target.matches(".drop-btn")) {
+        if (dropContent[i].classList.contains("show")) {
+          dropContent[i].classList.remove("show");
+        }
+      }
+    };
+  }
+
+  /*show 1*/
+
+  var i;
+
+  // Show content when the corespondent link is clicked
+  for (i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("click", selectItem);
+  }
+  function selectItem(item) {
+    // Initialize Remove all content
+    removeShow();
+    //Query Select the specific content eg for(home, about, doctrine, more)
+    const content = document.querySelector(`#${this.classList[1]}-content`);
+    //Adds the class with the display show
+    content.classList.add("show");
+    menu.style.maxHeight = null;
+    menu.style.transitionDuration = "0.1s";
+  }
+
+  // Removes all content
+  function removeShow() {
+    for (i = 0; i < showcase.length; i++) {
+      showcase[i].firstElementChild.classList.remove("show");
+    }
+  }
+
+  // Collapsible
+
+  var coll = document.getElementsByClassName("collapsible");
+  let collapsed = true;
+
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", toggleColl);
+  }
+
+  function toggleColl() {
+    var toggled = document.getElementById(this.id + "-content");
+
+    if (collapsed == true) {
+      toggled.style.maxHeight = toggled.scrollHeight + "px";
+      toggled.style.overflow = "hidden";
+      this.innerHTML = "Ascunde Reguliile";
+      collapsed = false;
+    } else {
+      if (x.matches) {
+        console.log("x.matches");
+        toggled.style.maxHeight = "300px";
+        toggled.style.overflowY = "scroll";
+      } else if (!x.matches) {
+        console.log("x.not matches");
+        toggled.style.maxHeight = "125px";
+        toggled.style.overflow = "hidden";
+      }
+      this.innerHTML = "Află mai multe";
+      collapsed = true;
+    }
+  }
+
+
+document.cookie = " Max-Age=2600000; Secure";
+
+
+// checks the device's OS
+var md = new MobileDetect(window.navigator.userAgent);
+console.log( md.os() );
+
+
+// Code for PC
+if(md.os() == null){
+// Capturing back on webpage
+// initiate the hash as empty array
+let hashHistory = [];
+
+// Checks if the first # is home
+if(window.location.hash == '#home'){
+  hashHistory.push('#home');
+} else {
+  window.location.hash = '#home';
+}
+
+let pageOut = false;
+document.body.onmouseleave = function() {
+  pageOut = true;
+  console.log(pageOut);
+}
+
+document.body.onmouseenter = function() {
+  pageOut = false;
+  console.log(pageOut);
+}
+
+window.onhashchange = function() {
+  // console.log(hashHistory);
+    if(pageOut == true && hashHistory.length >= 2) {
+      console.log('it works');
+      let item = hashHistory[hashHistory.length - 2];
+      console.log(item);
+      hashHistory.pop();
+      selectItemOnBack(item);
+  } else if(pageOut != true){
+    hashHistory.push(window.location.hash);
+    console.log(window.location.hash);
+    console.log(hashHistory);
+    console.log(window.location);
+  }
+
+  }
+
+ }
+// END of code for PC's
+else {
+  let hashHistory = [];
+  
+   if(window.location.hash == '#home'){
+     hashHistory.push('#home');
+    } else {
+     window.location.hash = '#home';
+    }
+    
+    let innerClick = false; 
+    document.body.onclick = () => {
+      innerClick = true;
+    }
+    window.addEventListener('popstate', () => {
+
+      if(innerClick == false){
+          catchBack();
+        } if(innerClick == true) {
+          hashHistory.push(window.location.hash);
+          console.log(hashHistory);
+        }
+        innerClick = false;
+      });
+      
+      function catchBack (){
+        console.log('Popstate works');
+        if(hashHistory.length >= 2) {
+        let item = hashHistory[hashHistory.length - 2];
+        console.log(item);
+        selectItemOnBack(item);
+        hashHistory.pop();
+        }
+      }
+      
+    }
+    
+    
+    
+    //  Select item
+     function selectItemOnBack(item) {
+      // Initialize Remove all content
+      removeShowOnBack();
+      //Query Select the specific content eg for(home, about, doctrine, more)
+      const content = document.querySelector(`${item}-content`);
+      //Adds the class with the display show
+      content.classList.add("show");
+      menu.style.maxHeight = null;
+      menu.style.transitionDuration = "0.5s";
+    }
+    
+    // Removes all content
+    function removeShowOnBack() {
+      for (i = 0; i < showcase.length; i++) {
+        showcase[i].firstElementChild.classList.remove("show");
+      }
+    }
+    
+    
+  } //media query - END
+  // Media Query init and callback
+  var x = window.matchMedia("(min-width: 784px)");
+  mediaQuery(x);
+  x.addListener(mediaQuery);
+
+  console.log(history);
+  history.scrollRestoration = 'manual';
+  
+  
